c5ec758058e1fea783eb89f0262402305b2ecb18
FatSandwich <63752541+FatSandwich@users.noreply.github.com>
2021-03-04 09:21:39 -0500

    Update app.py

--- app.py
+++ app.py
@@ -24,7 +24,7 @@ def process():
   soup = BeautifulSoup(r.text, 'html.parser')  
 
   cryptoname = soup.find('p', {
-      'class': 'sc-1eb5slv-0 hNpJqV converter-item-name'
+      'class': 'sc-AxhUy ghbWav converter-item-name'
   }).text
   
 
